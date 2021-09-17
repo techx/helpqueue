8916b2b5ebfde285a2932e30666ecfe33bfe11f6
Natalie <natalie.huang99@gmail.com>
2021-09-17 02:59:37 -0400

    event name

--- prebuild.py
+++ prebuild.py
@@ -25,7 +25,7 @@ from server.controllers.settings import *
 
 for setting in ALLDEFAULTSETTINGS:
   if (get_setting(None,setting, True) is None):
-    set_setting(None, setting, "CHANGE ME", override=True)
+    set_setting(None, setting, "HackMIT 2021", override=True)
 
 if (get_setting(None, SETTING_OFFICIAL_MESSAGE, True) is None):
   set_setting(None, SETTING_OFFICIAL_MESSAGE, "", override=True)
